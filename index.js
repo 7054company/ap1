72fcadc5507ea27bba0ec1c5ff16a1c56eefd053
VIKAS YADAV <34761088+7054company@users.noreply.github.com>
2025-01-12 14:20:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get(
 
 // Callback route where GitHub redirects after user login
 app.get(
-  '/auth/callback',
+  '/auth/callback1',
   passport.authenticate('oauth2', { failureRedirect: '/' }),
   async (req, res) => {
     try {
@@ -128,6 +128,32 @@ app.get(
     }
   }
 );
+
+
+
+app.get('/auth/callback', (req, res) => {
+  // The req.query object has the query params that
+  // were sent to this route. We want the `code` param
+  const requestToken = req.query.code
+  axios({
+    // make a POST request
+    method: 'post',
+    // to the Github authentication API, with the client ID, client secret
+    // and request token
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+    // Set the content type header, so that we get the response in JSOn
+    headers: {
+      accept: 'application/json'
+    }
+  }).then((response) => {
+    // Once we get the response, extract the access token from
+    // the response body
+    const accessToken = response.data.access_token
+    // redirect the user to the welcome page, along with the access token
+    res.redirect(`/profile?access_token=${accessToken}`)
+  })
+})
+
 
 // Profile route to display authenticated user info
 app.get('/profile', async (req, res) => {
